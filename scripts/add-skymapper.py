8b3472b18d99e37c0a4a502ae6ae25cec3c4686b
Michael S. P. Kelley <msk@astro.umd.edu>
2024-04-02 08:38:26 -0400

    SkyMapper DR4

--- scripts/add-skymapper.py
+++ scripts/add-skymapper.py
@@ -8,16 +8,19 @@ import numpy as np
 
 from sbsearch.logging import ProgressTriangle
 from catch import Catch
-from catch.model import SkyMapper
+from catch.model import SkyMapperDR4
 from catch.config import Config
 
 parser = argparse.ArgumentParser(
-    "add-skymapper", description="Add SkyMapper DR2 metadata to CATCH."
+    "add-skymapper", description="Add SkyMapper DR4 metadata to CATCH."
 )
 parser.add_argument("image_table", help="image (exposure) table")
-parser.add_argument("ccd_table", help="ccd table")
+parser.add_argument("ccd_tables", nargs="+", help="ccd tables")
 parser.add_argument("--config", help="CATCH configuration file")
 parser.add_argument("--debug", action="store_true", help="debug mode")
+parser.add_argument(
+    "--noop", action="store_true", help="no-operation mode, just process the files"
+)
 
 args = parser.parse_args()
 
@@ -62,46 +65,60 @@ for row in get_rows(args.image_table):
 
 # connect to catch database
 with Catch.with_config(Config.from_args(args)) as catch:
-    catch.db.drop_spatial_index()
+    if args.noop:
+        catch.logger.info(
+            "No-operation mode; not adding files to the database or computing spatial indices."
+        )
+    else:
+        catch.db.drop_spatial_index()
+
     observations = []
     count = 0
-    tri = ProgressTriangle(1, catch.logger, base=2)
-    # iterate over rows in CCD table
-    for row in get_rows(args.ccd_table):
-        # image_id,ccd,filename,maskname,image,filter,mjd_obs,fwhm,elong,
-        # nsatpix,sb_mag,phot_nstar,header,coverage
-        image = images[row["image_id"]]
-        ra, dec = np.degrees(cov2fov(row["coverage"]))
-        sb_mag = None if row["sb_mag"] == "" else float(row["sb_mag"])
+    # iterate over files
+    for ccd_table in args.ccd_tables:
+        tri = ProgressTriangle(1, catch.logger, base=2)
+        # iterate over rows in CCD table
+        for row in get_rows(ccd_table):
+            # image_id,ccd,filename,maskname,image,filter,mjd_obs,fwhm,elong,
+            # nsatpix,sb_mag,phot_nstar,header,coverage
+            image = images[row["image_id"]]
+            ra, dec = np.degrees(cov2fov(row["coverage"]))
+            sb_mag = None if row["sb_mag"] == "" else float(row["sb_mag"])
+            field_id = int(image["field_id"]) if image["field_id"] != "" else None
 
-        # SkyMapper object inherits sbsearch.schema.Obs columns.
-        # Note that 'source' and 'obsid' will be defined via sqlalchemy's polymorphism machinery.
-        obs = SkyMapper(
-            id=int(row["image"].replace("-", "")),
-            mjd_start=float(row["mjd_obs"]),
-            mjd_stop=float(row["mjd_obs"]) + float(image["exp_time"]) / 86400,
-            filter=row["filter"],
-            exposure=float(image["exp_time"]),
-            seeing=float(row["fwhm_ccd"]),
-            airmass=image["airmass"],
-            product_id=row["image"],
-            sb_mag=sb_mag,
-            field_id=int(image["field_id"]),
-            image_type=image["image_type"],
-            zpapprox=image["zpapprox"],
-        )
-        obs.set_fov(ra, dec)
-        observations.append(obs)
+            # SkyMapperDR4 object inherits sbsearch.schema.Obs columns.
+            # Note that 'source' and 'obsid' will be defined via sqlalchemy's polymorphism machinery.
+            obs = SkyMapperDR4(
+                source_id=int(row["image"].replace("-", "")),
+                mjd_start=float(row["mjd_obs"]),
+                mjd_stop=float(row["mjd_obs"]) + float(image["exp_time"]) / 86400,
+                filter=row["filter"],
+                exposure=float(image["exp_time"]),
+                seeing=float(row["fwhm_ccd"]),
+                airmass=image["airmass"],
+                product_id=row["image"],
+                sb_mag=sb_mag,
+                field_id=field_id,
+                image_type=image["image_type"],
+                zpapprox=image["zpapprox"],
+            )
+            obs.set_fov(ra, dec)
+            observations.append(obs)
 
-        # add 10000 at a time
-        tri.update()
-        if tri.i % 10000 == 0:
+            # add 10000 at a time
+            tri.update()
+            count += 1
+            if tri.i % 10000 == 0:
+                if not args.noop:
+                    catch.add_observations(observations)
+                observations = []
+
+        # add any remaining files
+        if not args.noop:
             catch.add_observations(observations)
-            observations = []
 
-    # add any remaining files
-    catch.add_observations(observations)
+    if not args.noop:
+        catch.db.create_spatial_index()
+        catch.update_statistics(source="skymapper_dr4")
 
-    catch.db.create_spatial_index()
-
-    catch.update_statistics(source="skymapper")
+    catch.logger.info(f"Processed %d images.", count)
