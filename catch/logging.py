49e8dccd061134c2eadfb5e74fac698834f2bc26
Michael S. P. Kelley <msk@astro.umd.edu>
2024-12-09 13:52:27 -0500

    Reformat a few lines

--- catch/logging.py
+++ catch/logging.py
@@ -9,13 +9,13 @@ class TaskMessenger:
 
     def __init__(self, job_id: uuid.UUID, debug: bool = False) -> None:
         job_hex: str = uuid.UUID(str(job_id), version=4).hex
-        self.logger: logging.Logger = logging.getLogger(
-            'CATCH-APIs {}'.format(job_hex))
+        self.logger: logging.Logger = logging.getLogger("CATCH-APIs {}".format(job_hex))
         self.logger.setLevel(logging.DEBUG if debug else logging.INFO)
         if len(self.logger.handlers) == 0:
             # always log to the console
             formatter: logging.Formatter = logging.Formatter(
-                '%(levelname)s %(asctime)s ({}): %(message)s'.format(job_hex))
+                "%(levelname)s %(asctime)s ({}): %(message)s".format(job_hex)
+            )
             console: logging.StreamHandler = logging.StreamHandler()
             console.setFormatter(formatter)
             self.logger.addHandler(console)
